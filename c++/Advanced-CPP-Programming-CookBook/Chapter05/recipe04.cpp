5505c0db0648cad08d424f632c7dc12fb8fded73
YanyangChen <184536632@qq.com>
2020-08-03 11:18:43 -0400

    added thread call template member function solution

--- c++/Advanced-CPP-Programming-CookBook/Chapter05/recipe04.cpp
+++ c++/Advanced-CPP-Programming-CookBook/Chapter05/recipe04.cpp
@@ -35,11 +35,90 @@ class my_stack
 
 public:
 
+    int counter = 0;
+    int sum = 0;
     template<typename ARG>
     void push(ARG &&arg)
     {
         std::lock_guard lock(m);
+	sum += std::forward<ARG>(arg);
 	std::cout << std::forward<ARG>(arg) << "\n" << static_cast<void*>(this) << "\n";
+
+
+	counter++;
+        m_stack.push(std::forward<ARG>(arg));
+    }
+
+    void pushint(int i){
+    push(i);
+    }
+    
+    void pop()
+    {
+        std::lock_guard lock(m);
+	std::cout << "cout top " << m_stack.top() << "\n"; 
+        m_stack.pop();
+    }
+
+    auto empty() const
+    {
+        std::lock_guard lock(m);
+        return m_stack.empty();
+    }
+};
+
+int main(void)
+{
+    my_stack<int> s;
+    int a = 4;
+    //stackoverflow.com/questions/21617860
+    std::thread t1([&]{ s.push(4); });
+    std::thread t2([&]{ s.push(5); });
+    std::thread t3([&]{ s.push(6); });
+    std::thread t4([&]{ s.push(7); });
+    std::thread t5([&]{ s.push(8); });
+    std::thread t6([&]{ s.push(9); });
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
+    t6.join();
+    while(!s.empty()) {
+        s.pop();
+    }
+    std::cout << s.counter << "\n" << s.sum << "\n";
+    return 0;
+}
+
+#endif
+// -----------------------------------------------------------------------------
+#ifdef EXAMPLE02
+
+#include <mutex>
+#include <stack>
+#include <iostream>
+#include <thread>
+
+template<typename T>
+class my_stack
+{
+    std::stack<T> m_stack;
+    mutable std::mutex m{};
+
+public:
+
+    int counter = 0;
+    int sum = 0;
+    template<typename ARG>
+    void push(ARG &&arg)
+    {
+        std::lock_guard lock(m);
+	sum += std::forward<ARG>(arg);
+	std::cout << std::forward<ARG>(arg) << "\n" << static_cast<void*>(this) << "\n";
+
+
+	counter++;
         m_stack.push(std::forward<ARG>(arg));
     }
 
@@ -65,9 +144,12 @@ int main(void)
 {
     my_stack<int> s;
 
-//    std::thread t1(&my_stack<int>::pushint, s, 4);
-//    std::thread t2(&my_stack<int>::pushint, s, 8);
-//    std::thread t3(&my_stack<int>::pushint, s, 15);
+   // std::thread t1(&my_stack<int>::pushint, &s, 4);
+    //std::thread t2(&my_stack<int>::pushint, &s, 8);
+    //std::thread t3(&my_stack<int>::pushint, &s, 15);
+    //std::thread t4(&my_stack<int>::pushint, &s, 16);
+    //std::thread t5(&my_stack<int>::pushint, &s, 23);
+    //std::thread t6(&my_stack<int>::pushint, &s, 42);
     s.push(4);
     s.push(8);
     s.push(15);
@@ -77,10 +159,13 @@ int main(void)
     //t1.join();
     //t2.join();
     //t3.join();
+    //t4.join();
+    //t5.join();
+    //t6.join();
     while(!s.empty()) {
         s.pop();
     }
-
+    std::cout << s.counter << "\n" << s.sum << "\n";
     return 0;
 }
 
