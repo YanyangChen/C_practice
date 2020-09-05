7fa068fe8e4e5353c12fbd818d644e71c306cbdc
chenyanyang <184536632@qq.com>
2020-09-05 19:19:35 +0800

    .

--- Algo/graphs/GS.cpp
+++ Algo/graphs/GS.cpp
@@ -63,7 +63,49 @@ void Graph::addEdge(int v, int w)
     adj[w].push_back(v); // Add w to v’s list. 
     }
 } 
+
+bool Graph::isCyclicUtil(int v, bool visited[], int parent) 
+{ 
+    // Mark the current node as visited 
+    visited[v] = true; 
   
+    // Recur for all the vertices adjacent to this vertex 
+    list<int>::iterator i; 
+    for (i = adj[v].begin(); i != adj[v].end(); ++i) 
+    { 
+        // If an adjacent is not visited, then recur for that adjacent 
+        if (!visited[*i]) 
+        { 
+           if (isCyclicUtil(*i, visited, v)) 
+              return true; 
+        } 
+  
+        // If an adjacent is visited and not parent of current vertex, 
+        // then there is a cycle. 
+        else if (*i != parent) 
+           return true; 
+    } 
+    return false; 
+} 
+
+bool Graph::isCyclic() 
+{ 
+    // Mark all the vertices as not visited and not part of recursion 
+    // stack 
+    bool *visited = new bool[V]; 
+    for (int i = 0; i < V; i++) 
+        visited[i] = false; 
+  
+    // Call the recursive helper function to detect cycle in different 
+    // DFS trees 
+    for (int u = 0; u < V; u++) 
+        if (!visited[u]) // Don't recur for u if it is already visited 
+          if (isCyclicUtil(u, visited, -1)) 
+             return true; 
+  
+    return false; 
+}
+
 void Graph::DFSUtil(int v, bool visited[]) 
 { 
     // Mark the current node as visited and 
@@ -180,7 +222,9 @@ int main(int argc, char *argv[])
     //g.addEdge(3, 3); 
   
     cout << " DFS";
-    g.DFS(0); 
+    g.DFS(0);
+    g1.isCyclic()? cout << "Graph contains cycle\n": 
+                   cout << "Graph doesn't contain cycle\n";
     cout << "\n";
     cout << " BFS";  
     g.BFS(4);
